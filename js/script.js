93759f111e4b80136c7e45d054e2d418a2318852
saroareee <saroareee@gmail.com>
2023-08-28 17:15:24 +0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        dm: ["DM Sans"],
+        chivo: ["Chivo"],
+      },
+      fontSize: {
+        56: "56px",
+        46: "46px",
+        44: "44px",
+        22: "22px",
+      },
+      colors: {
+        primary: "#3639A4",
+        secondary: "#43E7DF",
+        third: "#645E76",
+        highlight: "#FDB400",
+        border: "#DCE7FE",
+      },
+      spacing: {
+        26: "26px",
+        30: "30px",
+        50: "50px",
+        107: "107px",
+        100: "100px",
+        116: "116px",
+        140: "140px",
+        162: "162px",
+        180: "180px",
+        195: "195px",
+        200: "200px",
+        220: "220px",
+        230: "230px",
+        254: "254px",
+        291: "291px",
+      },
+      maxWidth: {
+        container: "1410px",
+      },
+      lineHeight: {
+        58: "58px",
+        80: "80px",
+      },
+    },
+  },
+};
